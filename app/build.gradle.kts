30098117fdaf12bd7be83a23c7abb5e1b6d5dd16
skylontechinc@gmail.com <skylontechinc@gmail.com>
2023-11-02 12:41:12 -0400

    Adding required dependencies and Added UnSplash access key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
+
+val unsplash_access_key: String = gradleLocalProperties(rootDir).getProperty("unsplash_access_key")
 
 android {
     namespace = "com.example.imagesearch"
@@ -15,6 +20,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "UNSPLASH_ACCESS_KEY", unsplash_access_key)
     }
 
     buildTypes {
@@ -33,15 +39,42 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompatç:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val daggerVersion = "2.48.1"
+
+    // dagger core
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+
+    // Android Dagger
+    implementation("com.google.dagger:dagger-android:$daggerVersion")
+    implementation("com.google.dagger:dagger-android-support:$daggerVersion") // if you use the support libraries
+    kapt("com.google.dagger:dagger-android-processor:$daggerVersion")
+
+    // retrofit
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Glide
+    val glideVersion = "4.14.2"
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+}
+
+kapt {
+    correctErrorTypes = true
 }
